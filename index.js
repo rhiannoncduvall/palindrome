3c1dffe407d969d64eefa021e947c4d25226e7ab
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-09-18 21:03:08 -0700

    basic functionality. function doesn't include edge cases. css not completed

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-isPalindrome = function() {
+const isPalindrome = function() {
+    // store input as a variable
+    let input = document.getElementById("submitted-text").value;
+    // console.log(input);
 
+    // initialize variable for reverse input
+    let reverseInput = "";
+
+    // iterate through each letter of the input starting from the last character
+    for (let i = input.length - 1; i >= 0; i--) {
+        reverseInput += input[i];
+    }
+
+    // compare the variables (input vs new)
+    if (input === reverseInput) {
+        // return true and edit HTML so it is on the true list
+        let item1 = document.createElement("LI");
+        item1.innerHTML = input;
+        document.getElementById("palindrome-list").appendChild(item1);
+        return true;
+    } else {
+        // return false and edit HTML so it is on the false list
+        let item2 = document.createElement("LI");
+        item2.innerHTML = input;
+        document.getElementById("not-palindrome-list").appendChild(item2);
+    }
 }
-
-// store input as a variable
-
-// reverse input and store as another variable
-
-// compare the variables
-
-// if variables match, return true and edit html
-
-// if variables do not match, return false and edit html
