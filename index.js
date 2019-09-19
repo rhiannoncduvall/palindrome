764d1e69b56f45ff766abf95377d0a7625c6a4a2
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-09-18 18:48:35 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+isPalindrome = function() {
+
+}
+
+// store input as a variable
+
+// reverse input and store as another variable
+
+// compare the variables
+
+// if variables match, return true and edit html
+
+// if variables do not match, return false and edit html
